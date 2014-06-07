c6d265b7f1321438012ba37d14e2e8dcddc3c80e
juanka.kode <juanka.kode@gmail.com>
2014-06-07 03:57:30 -0500

    replace bad regex which makes app hang when processing output from msbuild

--- src/Drone/Drone.Lib/Compilers/MSBuildHelper.cs
+++ src/Drone/Drone.Lib/Compilers/MSBuildHelper.cs
@@ -7,27 +7,19 @@ namespace Drone.Lib.Compilers
 {
 	public class MSBuildHelper
 	{
-		public static readonly string NormalErrorPattern = @"(?<file>.*)\((?<line>\d+),(?<column>\d+)\):\s+(?<error>\w+)\s+(?<number>[\d\w]+):\s+(?<message>.*)";
-		public static readonly string GeneralErrorPattern = @"(?<error>.+?)\s+(?<number>[\d\w]+?):\s+(?<message>.*)";
-
-		public static readonly Regex NormalErrorRegex = new Regex(NormalErrorPattern, RegexOptions.Compiled);
-		public static readonly Regex GeneralErrorRegex = new Regex(GeneralErrorPattern, RegexOptions.Compiled);
+		public static readonly string OutputPattern = @"^(?<origin>.*)\:\s+(?<subcategory>.*)(?<category>error|warning)\s+(?<code>[a-zA-Z]+\d+)\:(?<text>.*)$";
+		public static readonly Regex OutputRegex = new Regex(OutputPattern, RegexOptions.Compiled | RegexOptions.Singleline);
 
 		public static MSBuildOutputLevel GetOutputLevel(string data)
 		{
 			if (string.IsNullOrWhiteSpace(data))
 				return MSBuildOutputLevel.Normal;
 
-			var match = NormalErrorRegex.Match(data);
+			var match = OutputRegex.Match(data);
 
-			if (!match.Success)
-			{
-				match = GeneralErrorRegex.Match(data);
-			}
-
-			if (!match.Success)
+			if(!match.Success)
 				return MSBuildOutputLevel.Normal;
-			
+
 			var category = match.Groups["error"].Value;
 
 			if(category == "warning")
